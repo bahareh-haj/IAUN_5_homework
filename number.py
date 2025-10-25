61d3ac93c52e22bdec909aa820f6a3e16832dec8
bahareh <bhajiheidari@gmail.com>
2025-10-25 21:57:55 +0330

    number

---
+++ number.py
@@ -0,0 +1,10 @@
+number= input("enter number : ")
+if len(number) ==2 and number.isdigit():
+    num1=int(number[0])
+    num2=int(number[1])
+    result1=num1**num2
+    result2=num2**num1
+    print(f"first digit to the power of second digit : {result1}")
+    print(f"secend digit to the power of first digit : {result2}")
+else :
+    print("pleas enter a true number (your age)")
